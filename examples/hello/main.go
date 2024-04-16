612ae76a4ba10fba5543aa010ddac615147698c6
Daniel Lewandowski <daniel.lewandowski@outlook.com>
2024-04-16 13:35:56 +0200

    exploring different concept

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -10,31 +10,50 @@ import (
 // myActor is a concrete implementation of the Receiver interface
 type myActor[T any] struct{}
 
-// NewMyReceiver creates a new instance of myActor
-func NewMyReceiver[T any]() typed.Receiver[T] {
-	return &myActor[T]{}
+// NewMyReceive creates a new instance of myActor
+func NewMyReceive[T any]() typed.Receive[T] {
+	return myActor[T]{}.Receive
 }
 
 // Receive implements the Receiver interface and receives messages
-func (a *myActor[T]) Receive(ctx context.Context, msg T) error {
+func (a myActor[T]) Receive(ctx context.Context, msg T) error {
 	slog.InfoContext(ctx, "Custom logic", slog.Any("msg", msg))
 	return nil
 }
 
+type functionActor[T any] struct {
+	f func(ctx context.Context, msg T) error
+}
+
+func (f functionActor[T]) Receive(ctx context.Context, msg T) error {
+	return f.f(ctx, msg)
+}
+
+func NewActorFromFunction[T any](f func(ctx context.Context, msg T) error) typed.Receive[T] {
+	return functionActor[T]{f: f}.Receive
+}
+
 func main() {
-	// Create a context with a timeout
+	// create a context with a timeout
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	//create system
+	// create system
 	system := typed.NewSystem()
 
 	// create actors
-	actorStringRef := typed.Start(system, ctx, func() typed.Receiver[string] {
-		return NewMyReceiver[string]()
+	actorStringRef := typed.Start(ctx, system, func() (typed.Receive[string], error) {
+		return NewMyReceive[string](), nil
 	})
-	intActorRef := typed.Start(system, ctx, func() typed.Receiver[int] {
-		return NewMyReceiver[int]()
+	intActorRef := typed.Start(ctx, system, func() (typed.Receive[int], error) {
+		return NewMyReceive[int](), nil
+	})
+
+	panicActor := NewActorFromFunction[string](func(ctx context.Context, msg string) error {
+		panic("total panic")
+	})
+	panicActorRef := typed.Start(ctx, system, func() (typed.Receive[string], error) {
+		return panicActor, nil
 	})
 
 	// Send string messages with the custom logic
@@ -44,8 +63,11 @@ func main() {
 	// Send integer messages without custom logic (uses default logic)
 	intActorRef.Send(ctx, 42)
 	intActorRef.Send(ctx, 100)
-
+	panicActorRef.Send(ctx, "nothing else matters")
+	time.Sleep(100 * time.Millisecond)
+	panicActorRef.Send(ctx, "nothing else matters")
 	time.Sleep(2 * time.Second)
+
 	cancel()
 
 	system.Wait()
--- typed/system.go
+++ typed/system.go
@@ -4,93 +4,98 @@ import (
 	"context"
 	"errors"
 	"log/slog"
-	"runtime/debug"
 	"sync"
 )
 
 var ErrActorPanicked = errors.New("actor panicked")
-
-type System interface {
-	Wait()
-	waitGroup() *sync.WaitGroup
-}
+var ErrMailboxClosed = errors.New("mailbox closed")
 
 type system struct {
-	wg *sync.WaitGroup
+	wg sync.WaitGroup
 }
 
 func (s *system) waitGroup() *sync.WaitGroup {
-	return s.wg
+	return &s.wg
 }
 
 func (s *system) Wait() {
 	s.wg.Wait()
 }
 
-var _ System = (*system)(nil)
-
 func NewSystem() System {
-	return &system{wg: &sync.WaitGroup{}}
+	return &system{}
 }
 
-// Start starts the actor by running a goroutine that listens for messages on the mailbox
-func Start[T any](system System, ctx context.Context, spawn func() Receiver[T]) Actor[T] {
-	mailbox := make(chan envelope[T])
-	done := make(chan struct{})
-
-	go forever(system.waitGroup(), ctx, spawn, mailbox, done)
-
-	return &actor[T]{mailbox: mailbox, done: done}
-}
-
-func forever[T any](wg *sync.WaitGroup, ctx context.Context, spawn func() Receiver[T], mailbox chan envelope[T], done chan<- struct{}) {
-	defer func() {
-		if r := recover(); r != nil {
-			slog.ErrorContext(ctx, "forever loop panicked", slog.String("stack", string(debug.Stack())))
+// Start starts the actor by running a goroutine that listens for messages on the channel
+func Start[T any](ctx context.Context, system System, spawn Spawn[T]) Mailbox[T] {
+	ref := mailbox[T]{channel: make(chan envelope[T], 1)}
+	go func() {
+		if err := run(ctx, system.waitGroup(), spawn, ref); err != nil {
+			slog.ErrorContext(ctx, "forever loop errored", slog.Any("error", err))
 		}
 	}()
 
-	defer close(mailbox)
-	defer close(done)
+	return ref
+}
+
+func run[T any](ctx context.Context, wg *sync.WaitGroup, spawn Spawn[T], ref mailbox[T]) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			slog.ErrorContext(ctx, "forever loop panicked", slog.Any("recover", r))
+			err = errors.New("forever loop panicked")
+		}
+	}()
+
+	defer func() {
+		if err := ref.Close(); err != nil {
+			slog.ErrorContext(ctx, "mailbox close errored", slog.Any("error", err))
+		}
+	}()
 
 	wg.Add(1)
 	defer wg.Done()
 
-	var receiver Receiver[T]
+	for {
+		receiver, err := spawn()
+		if err != nil {
+			return errors.Join(errors.New("spawn function failed"), err)
+		}
+		err = withoutTimeout[T](ctx, ref.channel, receiver)
+		switch {
+		case errors.Is(err, ErrActorPanicked):
+			// restart an actor
+			slog.ErrorContext(ctx, "actor panicked - restart", slog.Any("error", err))
+			break
+		case !errors.Is(err, nil):
+			// stop an actor
+			return errors.Join(errors.New("actor errored - stop"), err)
+		}
+	}
+}
+
+func withoutTimeout[T any](ctx context.Context, mailbox chan envelope[T], receive Receive[T]) error {
 	for {
 		select {
 		case <-ctx.Done():
-			slog.ErrorContext(ctx, "context canceled")
-			return
+			return ctx.Err()
 		case msg, ok := <-mailbox:
 			if !ok {
-				slog.ErrorContext(ctx, "mailbox was closed")
-				return
-			}
-			if receiver == nil {
-				receiver = spawn()
+				return ErrMailboxClosed
 			}
 
-			err := func() (err error) {
-				defer func() {
-					if r := recover(); r != nil {
-						err = ErrActorPanicked
-					}
-				}()
-				return receiver.Receive(msg.ctx, msg.msg)
-			}()
-
-			switch {
-			case errors.Is(err, nil):
-				// do nothing - no error
-			case errors.Is(err, ErrActorPanicked):
-				// receiver panicked - restart it
-				slog.ErrorContext(ctx, "receiver panicked - restarting")
-				receiver = nil
-			default:
-				slog.ErrorContext(ctx, "receiver returned error")
-				return
+			if err := safeReceive(receive, msg); err != nil {
+				return err
 			}
 		}
 	}
+}
+
+func safeReceive[T any](receive Receive[T], msg envelope[T]) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = ErrActorPanicked
+		}
+	}()
+
+	return receive(msg.ctx, msg.msg)
 }
